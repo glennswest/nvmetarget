6fd14512732fe8478b9f0fbfe353f73141441307
gwest <glennswest@neuralcloudcomputing.com>
2025-03-20 23:19:48 -0500

    Add nvme discovery to test

--- test.sh
+++ test.sh
@@ -6,5 +6,6 @@ rmdir /sys/kernel/config/nvmet/subsystems/storage/namespaces/*
 rmdir /sys/kernel/config/nvmet/subsystems/*
 losetup -d /dev/loop0
 python tests/test_nvmelib.py 
+nvme discover --transport=tcp --traddr=192.168.1.51  --trsvcid=4420
 
 
