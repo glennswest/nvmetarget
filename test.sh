35e251f021b9d2f99ac5213fa645e461c6b06e3f
gwest <glennswest@neuralcloudcomputing.com>
2025-03-20 22:52:46 -0500

    Tested/working for 1 device/export, 2 is broken

---
+++ test.sh
@@ -0,0 +1,10 @@
+./build.sh
+rm /etc/nvmetarget.json
+rm /sys/kernel/config/nvmet/ports/1/subsystems/storage
+rmdir /sys/kernel/config/nvmet/ports/1
+rmdir /sys/kernel/config/nvmet/subsystems/storage/namespaces/*
+rmdir /sys/kernel/config/nvmet/subsystems/*
+losetup -d /dev/loop0
+python tests/test_nvmelib.py 
+
+
