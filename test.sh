877f0b657ca85e65c0b464e466fc382835de127d
gwest <glennswest@neuralcloudcomputing.com>
2025-03-28 10:49:12 -0500

    Cleanup test script

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ rm /sys/kernel/config/nvmet/ports/1/subsystems/*
 rmdir /sys/kernel/config/nvmet/ports/1
 rmdir /sys/kernel/config/nvmet/subsystems/*/namespaces/*
 rmdir /sys/kernel/config/nvmet/subsystems/*
-losetup -d /dev/loop0
 python tests/test_nvmelib.py 
 nvme discover --transport=tcp --traddr=192.168.1.51  --trsvcid=4420
 modprobe nvme-tcp
